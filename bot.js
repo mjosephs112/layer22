7a20e7d8c727f98be586a639902c86602b4468eb
mjosephs112 <78871442+mjosephs112@users.noreply.github.com>
2021-02-13 05:14:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  status: 'dnd',
+   bot.user.setPresence({
+        game: {
+            type: "dnd",
+        }
 });
 
 client.login(process.env.BOT_TOKEN)
