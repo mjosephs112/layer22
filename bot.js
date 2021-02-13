b70ee78e9ba8051f226003d86ea1e7287e8073b2
mjosephs112 <78871442+mjosephs112@users.noreply.github.com>
2021-02-13 04:47:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,10 +4,9 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-	
+{
 	bot.user.setStatus('layer22.jp')
-
-  }
+}
 });
 
 client.login(process.env.BOT_TOKEN);
